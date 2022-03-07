9b5fdc51f85fab15338b352a4179aedca96b3b7a
Dimitri Tsardakas <dimitri.tsardakas@gmail.com>
2022-03-07 12:11:40 +1100

    Installed firebase and next-auth, wrapped app in session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import { SessionProvider } from 'next-auth/react';
+
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// Destructure page props to get the session data
+// SessionProvider also takes care of keeping the session updated
+// and synced between browser tabs and windows.
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
