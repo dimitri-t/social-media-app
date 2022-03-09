a7a9fd61ccb742354f4ceed5c590e0f4a5e527b0
Dimitri Tsardakas <dimitri.tsardakas@gmail.com>
2022-03-09 23:33:57 +1100

    Implemented comments and likes functionality

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import {
   BookmarkIcon,
   DotsHorizontalIcon,
@@ -7,10 +7,81 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
 } from '@heroicons/react/outline';
-
+import { useSession } from 'next-auth/react';
 import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
+import {
+  addDoc,
+  serverTimestamp,
+  collection,
+  onSnapshot,
+  orderBy,
+  setDoc,
+  doc,
+  query,
+  deleteDoc,
+} from 'firebase/firestore';
+import { db } from '../firebase';
 
 function Post({ id, username, avatar, img, caption }) {
+  const { data: session } = useSession();
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  // load likes
+  useEffect(
+    () =>
+      onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uuid));
+    } else {
+      await setDoc(doc(db, 'posts', id, 'likes', session.user.uuid), {
+        username: session.user.username,
+      });
+    }
+  };
+
+  // load comments
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, 'posts', id, 'comments'),
+          orderBy('timestamp', 'descending')
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+    const commentToSend = comment;
+    setComment('');
+    await addDoc(collection(db, 'posts', id, 'comments'), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  };
+
+  // seraches if the current user id is in the likes array
+  useEffect(
+    () =>
+      setHasLiked(
+        likes.findIndex((like) => like.id === session?.user?.uuid) !== -1
+      ),
+    [likes]
+  );
+
   return (
     <div className='bg-white my-7 border rounded-sm'>
       {/* Header part of the post */}
@@ -28,32 +99,67 @@ function Post({ id, username, avatar, img, caption }) {
       <img src={img} className='object-cover w-full' alt='' />
 
       {/* Buttons */}
-      <div className='flex justify-between px-4 pt-4'>
-        <div className='flex space-x-4'>
-          <HeartIcon className='btn' />
-          <ChatIcon className='btn' />
-          <PaperAirplaneIcon className='btn' />
+      {session && (
+        <div className='flex justify-between px-4 pt-4'>
+          <div className='flex space-x-4'>
+            {hasLiked ? (
+              <HeartIconFilled onClick={likePost} className='btn' />
+            ) : (
+              <HeartIcon onClick={likePost} className='btn' />
+            )}
+            <ChatIcon className='btn' />
+            <PaperAirplaneIcon className='btn' />
+          </div>
+          <BookmarkIcon className='btn' />
         </div>
-        <BookmarkIcon className='btn' />
-      </div>
+      )}
 
       {/* Caption */}
       <p className='p-5 truncate'>
+        {likes.length > 0 && <p className='font-bold'>{likes.length} likes</p>}
         <span className='font-bold mr-1'>{username} </span> {caption}
       </p>
 
       {/* Comments */}
+      {comments.length > 0 && (
+        <div className='ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin'>
+          {comments.map((comment) => (
+            <div key={comment.id} className='flex items-center space-x-2 mb-3'>
+              <img
+                className='h-7 rounded-full'
+                src={comment.data().userImage}
+                alt=''
+              />
+              <p className='text-sm flex-1'>
+                <span className='font-bold'>{comment.data().username}</span>{' '}
+                {comment.data().comment}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/* Leave a comment box */}
-      <form className='flex items-center p-4'>
-        <EmojiHappyIcon className='h-7' />
-        <input
-          type='text'
-          placeholder='Add a comment...'
-          className='border-none flex-1 focus:ring-0 outline-none'
-        />
-        <button className='text-blue-400 font-bold prevent'>Post</button>
-      </form>
+      {session && (
+        <form className='flex items-center p-4'>
+          <EmojiHappyIcon className='h-7' />
+          <input
+            type='text'
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder='Add a comment...'
+            className='border-none flex-1 focus:ring-0 outline-none'
+          />
+          <button
+            type='submit'
+            disable={!comment.trim()}
+            onClick={sendComment}
+            className='text-blue-400 font-bold prevent'
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 }
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,20 +1,36 @@
 import Post from './Post';
 import { useState, useEffect } from 'react';
 import faker from '@faker-js/faker';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
 function Posts() {
   const [posts, setPosts] = useState([]);
 
-  // Nice way of generating fake data from faker and storing it to an
-  // array without using a for loop
-  useEffect(() => {
-    const fakePosts = [...Array(5)].map((_, i) => ({
-      ...faker.helpers.contextualCard(),
-      // Changing the id to the array index
-      id: i,
-    }));
-    setPosts(fakePosts);
-  }, []);
+  // Load posts from firebase
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          // snapshot is a real time listener, if its value changes in the db
+          // it will provide the new value
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
+  // // Nice way of generating fake data from faker and storing it to an
+  // // array without using a for loop
+  // useEffect(() => {
+  //   const fakePosts = [...Array(5)].map((_, i) => ({
+  //     ...faker.helpers.contextualCard(),
+  //     // Changing the id to the array index
+  //     id: i,
+  //   }));
+  //   setPosts(fakePosts);
+  // }, []);
 
   return (
     <div>
@@ -23,10 +39,10 @@ function Posts() {
           <Post
             key={post.id}
             id={post.id}
-            username={post.username}
-            avatar={post.avatar}
-            caption={post.caption}
-            img={post.avatar}
+            username={post.data().username}
+            avatar={post.data().profileImg}
+            caption={post.data().caption}
+            img={post.data().image}
           />
         );
       })}
